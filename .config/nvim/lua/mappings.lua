fb75d5a3d70de5eb447c5183e839fb34c6b895a5
Addison Beck <hello@addisonbeck.com>
2023-12-05 20:32:35 +0000

    Add tab mappings for telescope

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,3 +2,5 @@ local opts = { noremap=true, silent=true }
 local optss = { noremap=true, silent=true, unique=true }
 vim.api.nvim_set_keymap('n', '|', ':NvimTreeFindFileToggle<CR>', opts)
 vim.api.nvim_set_keymap('n', '\\', ':Telescope<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Tab>', ':Telescope buffers<CR>', opts)
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':Telescope git_files<CR>', opts)
