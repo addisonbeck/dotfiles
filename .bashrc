abcca713fe7387cb331489523c5099dad494abbc
Addison Beck <hello@addisonbeck.com>
2023-12-16 19:51:44 -0600

    Rework secrets a bit to support using multiple Bitwarden accoutns

---
+++ .bashrc
@@ -0,0 +1,3 @@
+source ~/.profile
+alias bw-p="BITWARDENCLI_APPDATA_DIR=~/.config/bw-p && $(which bw) $@"
+alias bw-w="BITWARDENCLI_APPDATA_DIR=~/.config/bw-w && $(which bw) $@"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
-  if test -e ~/.env
-      envsource ~/.env
+  if test -e ~/.profile
+      fenv "source ~/.profile"
   end
   alias fd="fd -uu"
+  alias bw-p="set -g -x BITWARDENCLI_APPDATA_DIR '~/.config/bw-p' && $(which bw) $argv"
+  alias bw-w="set -g -x BITWARDENCLI_APPDATA_DIR '~/.config/bw-w' && $(which bw) $argv"
   omf theme ''
   if test -e ~/.config/starship-installed
     starship init fish | source
