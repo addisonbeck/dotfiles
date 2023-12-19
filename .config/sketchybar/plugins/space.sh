35ce1df75bfa1bcbbc4a4628ad40ff92dbf9eda7
Addison Beck <hello@addisonbeck.com>
2023-12-18 20:19:21 -0600

    PoC for organizing fonts and colors

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+source "$HOME/bin/colors"
+
 # The $SELECTED variable is available for space components and indicates if
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
-sketchybar --set $NAME background.drawing=$SELECTED
+if [ "$SELECTED" = "true" ]; then
+  sketchybar --set $NAME icon.color=$YELLOWHEX
+else
+  sketchybar --set $NAME icon.color=$WHITEHEX
+fi
