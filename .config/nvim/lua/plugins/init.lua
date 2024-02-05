a7a93855a692f78857291c4f2da1d239c94b04da
Addison Beck <hello@addisonbeck.com>
2024-02-05 12:15:26 -0600

    Restructure work

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local enabled_plugins = {
+  require "plugins.fuzzy-search",
+  require "plugins.icons",
+  require "plugins.start-screen",
+  require "plugins.language-server-protocol",
+  require "plugins.syntax-highlighting",
+  require "plugins.quick-comments",
+  require "plugins.virtual-column",
+  require "plugins.focus-mode",
+}
+
+require("lazy").setup(enabled_plugins)
