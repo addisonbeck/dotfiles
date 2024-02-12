c31fdef9eccdf3b78b27b403e58da6168601a4c7
Addison Beck <hello@addisonbeck.com>
2024-02-12 17:47:28 -0600

    Only load neovim plugins that don't have syntax errors

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -12,17 +12,29 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Declare a list of currently in use plugins
 local enabled_plugins = {
-  require "plugins.fuzzy-search",
-  require "plugins.icons",
-  require "plugins.start-screen",
-  require "plugins.language-server-protocol",
-  require "plugins.syntax-highlighting",
-  require "plugins.quick-comments",
-  require "plugins.virtual-column",
-  require "plugins.focus-mode",
-  -- require "plugins.session-manager",
+  "fuzzy-search",
+  "icons",
+  "start-screen",
+  "language-server-protocol",
+  "syntax-highlighting",
+  "quick-comments",
+  "virtual-column",
+  "focus-mode",
+  --"plugins.session-manager",
 }
 
-require("lazy").setup(enabled_plugins)
+-- Only load plugins that don't have any syntax errors
+local functional_plugins = {}
+for i = 1, #enabled_plugins do
+  local pluginIsOk, plugin = pcall(require, 'plugins.' .. enabled_plugins[i])
+  if pluginIsOk then
+    table.insert(functional_plugins, plugin)
+  else
+    print("Module had an error")
+  end
+  i = i + 1
+end
 
+require("lazy").setup(functional_plugins)
